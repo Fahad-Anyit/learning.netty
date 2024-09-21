a43630c74bb5c47c204e216c5b97508377c6f5c4
FahadAnyit <fahad@jfreaks.com>
2024-09-21 18:11:54 +0500

    added echo server

--- src/main/java/com/example/netty/NettyApplication.java
+++ src/main/java/com/example/netty/NettyApplication.java
@@ -1,13 +1,52 @@
 package com.example.netty;
 
-import org.springframework.boot.SpringApplication;
+import com.example.netty.echoServer.channelHandlers.EchoServerChannelHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.net.InetSocketAddress;
 
 @SpringBootApplication
 public class NettyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NettyApplication.class, args);
+	public static void main(String[] args) throws InterruptedException {
+//		SpringApplication.run(NettyApplication.class, args);
+		int port = 8080;
+
+		new EchoServer(port).start();
+	}
+
+	public static final class EchoServer {
+		private final int port;
+
+		public EchoServer(int port) {
+			this.port = port;
+		}
+
+		public void start() throws InterruptedException {
+			final EchoServerChannelHandler echoServerChannelHandler = new EchoServerChannelHandler();
+			try (EventLoopGroup eventLoopGroup = new NioEventLoopGroup()) {
+				ServerBootstrap bootstrap = new ServerBootstrap();
+				bootstrap.group(eventLoopGroup)
+						.channel(NioServerSocketChannel.class)
+						.localAddress(port)
+						.childHandler(new ChannelInitializer<SocketChannel>() {
+							@Override
+							protected void initChannel(SocketChannel ch) throws Exception {
+								ch.pipeline().addLast(echoServerChannelHandler);
+							}
+						});
+				ChannelFuture channelFuture = bootstrap.bind().sync();
+				channelFuture.channel().closeFuture().sync();
+			}
+
+		}
 	}
 
 }
--- src/main/java/com/example/netty/echoServer/channelHandlers/EchoServerChannelHandler.java
+++ src/main/java/com/example/netty/echoServer/channelHandlers/EchoServerChannelHandler.java
@@ -6,6 +6,7 @@ import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,7 +18,7 @@ public class EchoServerChannelHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         var byteBuffer = (ByteBuf) msg;
-        System.out.println("message received: " + byteBuffer.toString());
+        System.out.println("message received: " + byteBuffer.toString(CharsetUtil.UTF_8));
         ctx.write(byteBuffer);
     }
 
